9ba4f60200ffe11e0809e4c48f60b63275e51a48
Daisu2001 <migueescobar01@gmail.com>
2026-07-23 17:06:51 -0500

    Remove Walker class and implement random Gaussian circle drawing in draw function

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Simulacion-2026
 
+## Actividad 1
+
+Vi los videos propuestos en la unidad como referentes, y me sirvieron para entender hasta donde puede llegar el arte generativo, me sirvió   como inspiración.
+
+
+## Actividad 2
+
+
+Analizamos el ejemplo del random walk, lo pude ejecutar y verlo funcionando en línea y local.
+
+## Actividad 3
+
+Analizamos las distribuciónes de probabilidad, vimos el ejemplo de una distribución gaussiana.
+
+Una distribución uniforme de la aleatorieidad implica que haya la misma probabilidad de obtener cualquiera de las posibles opciones, mientras que una distribución no uniforme favorece ciertos resultados por lo que al final obtendrías una línea con huecos o difuminada en alguna dirección.
+
+## Actividad 4
+
+Distribución normal.
+
--- sim2026-20-Test Miguel Escobar/Actividad2/sketch.js
+++ sim2026-20-Test Miguel Escobar/Actividad2/sketch.js
@@ -2,56 +2,15 @@
 // Daniel Shiffman
 // http://natureofcode.com
 
-let walker;
-
 function setup() {
   createCanvas(640, 240);
-  walker = new Walker();
   background(255);
-
-   
 }
 
 function draw() {
-
-  walker.step();
-  walker.show();
-}
-
-class Walker {
-  constructor() {
-    this.x = width / 2;
-    this.y = height / 2;
-    this.r = 8;
-    this.x2 = this.x - this.r;
-    this.y2 = this.y + this.r;
-    this.x3 = this.x + this.r;
-    this.y3 = this.y + this.r;
-  }
-
-  show() {
-    stroke(0);
-    triangle(this.x, this.y, this.x2, this.y2, this.x3, this.y3);
-  }
-
-  step() {
-
-    const choice = floor(random(4));
-    if (choice >2) {
-      this.x++;
-    
-    } else if (choice == 1) {
-      this.x--;
-    } else if (choice == 2) {
-      this.y++;
-    } else if (choice <= 2) {
-      this.y--;
-    }
-
-    // Recalculate triangle vertex coordinates around (this.x, this.y)
-    this.x2 = this.x - this.r;
-    this.y2 = this.y + this.r;
-    this.x3 = this.x + this.r;
-    this.y3 = this.y + this.r;
-  }
+  // A normal distribution with mean 320 and standard deviation 60
+  let x = randomGaussian(320, 60);
+  noStroke();
+  fill(0, 10);
+  circle(x, 120, 16);
 }
